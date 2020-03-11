983521575e66630911c250240396174619d91337
Gael Varoquaux <gael.varoquaux@normalesup.org>
2020-03-10 23:05:40 -0400

    Keep ISO

--- data_input.py
+++ data_input.py
@@ -8,18 +8,19 @@ def get_data():
     """ Download the data and return it as a 'wide' data frame
     """
     df = pycovid.getCovidCases()
+    df.rename(columns={"alpha-3": "iso"}, inplace=True)
     # The number of reported cases per day, country, and type
-    df_day = df.groupby(['country_region', 'date', 'type']).sum()
+    df_day = df.groupby(['country_region', 'iso', 'date', 'type']).sum()
 
     # %%
     # The cumulative sum
-    df_sum = df_day.groupby(['country_region', 'type']
+    df_sum = df_day.groupby(['country_region', 'iso', 'type']
                             ).transform(lambda x: x.cumsum())['cases']
     df_sum = df_sum.reset_index()
     # %%
     # Switch to wide format (time series)
     data = df_sum.pivot_table(values='cases',
-                              columns=['type', 'country_region'],
+                              columns=['type', 'iso', 'country_region'],
                               index=['date'])
     data = data.fillna(method='ffill')
     return data
