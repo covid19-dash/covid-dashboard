49bb88163987054a0cde5d844eedfa5890dcf125
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-03-18 10:43:41 +0100

    remove useless content

--- build_tools/circleci/post_processing.py
+++ build_tools/circleci/post_processing.py
@@ -25,27 +25,6 @@ def inject_index_html(filename):
         f.writelines(content)
 
 
-def inject_source_map(filename):
-    try:
-        with open(filename, "r") as f:
-            content = f.readlines()
-    except:
-        return
-
-    PATTERN = "sourceMappingURL="
-
-    for line_idx in range(len(content)):
-        content[line_idx] = content[line_idx].replace(
-            PATTERN, PATTERN + "/0/"
-        )
-
-    with open(filename, "w") as f:
-        f.writelines(content)
-
-
 if __name__ == "__main__":
     filename = os.path.join(sys.argv[1], "index.html")
     inject_index_html(filename)
-    for root, dirs, files in os.walk(sys.argv[1]):
-        for f in files:
-            inject_source_map(os.path.join(root, f))
